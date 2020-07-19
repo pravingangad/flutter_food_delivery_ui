2388f0f29375d68a684d7226dc042c32dcff07e2
pravin <gallopers@simsol.in>
2020-07-19 16:59:05 +0530

    folder delete

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/home/pravin/Documents/flutter"
+export "FLUTTER_APPLICATION_PATH=/home/pravin/AndroidStudioProjects/food delivery/flutter_food_delivery_ui"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
+export "FLUTTER_FRAMEWORK_DIR=/home/pravin/Documents/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
